b59e529eaf0cde4f057c883450f0fbffabe9ddff
xThiagoSant <uzmaki_thiago@outlook.com>
2021-11-03 17:34:03 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsvendas.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsvendas.dto.SaleDto;
+import com.devsuperior.dsvendas.services.SaleService;
+
+@RestController
+@RequestMapping(value = "/sales")
+public class SaleController {
+	
+	@Autowired
+	private SaleService saleService;
+	
+	@GetMapping
+	public ResponseEntity<Page<SaleDto>> findAll(Pageable pageable){
+		Page<SaleDto> result = saleService.findAll(pageable);
+		return ResponseEntity.ok(result);
+	}
+}
--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDto.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDto.java
@@ -1,5 +1,86 @@
 package com.devsuperior.dsvendas.dto;
 
+import java.time.LocalDate;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
 public class SaleDto {
 
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	private SellerDto seller;
+	
+	public SaleDto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SaleDto(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDto seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+	
+	public SaleDto(Sale entity) {
+		this.id = entity.getId();
+		this.visited = entity.getVisited();
+		this.deals = entity.getDeals();
+		this.amount = entity.getAmount();
+		this.date = entity.getDate();
+		this.seller =  new SellerDto(entity.getSeller());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public SellerDto getSeller() {
+		return seller;
+	}
+
+	public void setSeller(SellerDto seller) {
+		this.seller = seller;
+	}
 }

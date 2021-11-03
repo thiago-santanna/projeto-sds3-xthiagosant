0c4cf79c1349a600f1523ccf2376f1d2491579e8
xThiagoSant <uzmaki_thiago@outlook.com>
2021-11-03 16:42:51 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsvendas.dto.SellerDto;
+import com.devsuperior.dsvendas.services.SellerService;
+
+@RestController
+@RequestMapping(value = "/sellers")
+public class SellerController {
+
+	@Autowired
+	private SellerService sellerService;
+	
+	@GetMapping
+	public ResponseEntity<List<SellerDto>> findAll(){
+		List<SellerDto> result = sellerService.findAll();
+		return ResponseEntity.ok(result);
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDto;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	public List<SellerDto>	findAll(){
+		List<Seller> findAll = sellerRepository.findAll();
+		return findAll.stream().map(seller -> new SellerDto(seller)).collect(Collectors.toList());
+	}
+}

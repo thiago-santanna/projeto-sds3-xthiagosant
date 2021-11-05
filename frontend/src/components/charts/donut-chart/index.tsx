f80ce0b422afe636e3241e01a80240fdae41456e
xThiagoSant <uzmaki_thiago@outlook.com>
2021-11-05 13:00:14 -0300

    primeiro request, da forma errada de fazer em react, sem useState e useEffect

--- frontend/src/components/charts/donut-chart/index.tsx
+++ frontend/src/components/charts/donut-chart/index.tsx
@@ -1,22 +1,44 @@
+import axios from 'axios'
 import Chart from 'react-apexcharts'
+import { saleSum } from 'types/saleSum'
+import { URL_BASE_DEV, URL_BASE_PROD } from 'utils/request'
+
+type IChartData = {
+  labels: string[],
+  series: number[],
+}
 
 const DonutChart = () => {
 
-  const mockData = {
-      series: [477138, 499928, 444867, 220426, 473088],
-      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  }
+  //forma errada
+  let chartData: IChartData = { labels: [], series: [] }
+
+  //forma errada
+  axios.get(`${URL_BASE_PROD}/sales/amount-by-seller`)
+    .then(response => {
+      const data = response.data as saleSum[];
+      data.forEach(s => {
+        chartData.labels.push(s.sellerName);
+        chartData.series.push(s.sum);
+      })
+      console.log(chartData);
+    })
+
+  // const mockData = {
+  //   series: [477138, 499928, 444867, 220426, 473088],
+  //   labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+  // }
 
   const options = {
-      legend: {
-          show: true
-      }
+    legend: {
+      show: true
+    }
   }
 
   return (
-    <Chart 
-      options={{ ...options , labels: mockData.labels}}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,2 @@
+export const URL_BASE_DEV = 'https://localhost:8080;'
+export const URL_BASE_PROD = "https://sds5-xthiagosant.herokuapp.com";

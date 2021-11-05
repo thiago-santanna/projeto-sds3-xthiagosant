cd9fc293b138b7aa3b32b8d8b185a89b1a872e01
xThiagoSant <uzmaki_thiago@outlook.com>
2021-11-05 16:53:57 -0300

    variavel de ambiente

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,2 +1,1 @@
-export const URL_BASE_PROD = 'http://localhost:8080'
-//export const URL_BASE_PROD = "https://sds5-xthiagosant.herokuapp.com";
+export const URL_BASE_PROD = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
